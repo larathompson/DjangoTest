306eaf242094c4fe57c8216bacd25e33271624f0
Lara Thompson <larathompson288@gmail.com>
2020-12-10 14:05:28 +0000

    setupblog

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+  {
+    'author' : 'Corey',
+    'title' : 'Blog Post 1',
+    'content' : '1st post',
+    'date_posted' : 'Auguts 27, 2018'
+  },
+   {
+    'author' : 'Jane',
+    'title' : 'Blog Post 2',
+    'content' : '12jd post',
+    'date_posted' : 'Auguts 28, 2018'
+  },
+
+]
+
 
 # Create your views here.
 # this handles traffick from the blog
 #return an http response with an h1 tag
 
 def home(request):
-  return render(request, 'blog/home.html')
+  context = {
+    'posts' : posts
+  }
+  return render(request, 'blog/home.html', context)
 
 def about(request):
-  return HttpResponse('<h1>Blog About </>')
+  return render(request, 'blog/about.html', {'title': 'About'})
 
